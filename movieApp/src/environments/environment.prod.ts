55575d3aa15449a00f253a453d2415940bc31f01
Chris Keppler <ckkeppler@gmail.com>
2023-03-16 14:17:41 -0400

    prod fix

--- movieApp/src/environments/environment.prod.ts
+++ movieApp/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://api.themoviedb.org/3',
+  API_KEY: '1c18965da874445f0bd6b78f6f84faa2',
+  images: 'http://image.tmdb.org/t/p',
 };
